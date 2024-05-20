3906ea8f6d1d4bdbfc5485eb838a2df1441c4ac5
smcon <165791594+smcon@users.noreply.github.com>
2024-05-21 06:03:52 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題005</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 変数の代入
+    $score_01 = 80;
+    $score_02 = 60;
+    $score_03 = 55;
+    $score_04 = 40;
+    $score_05 = 100;
+    $score_06 = 25;
+    $score_07 = 80;
+    $score_08 = 95;
+    $score_09 = 30;
+    $score_10 = 60;
+    echo $score_01 + $score_02 + $score_03 + $score_04 +$score_05 + $score_06 + $score_07 + $score_08 + $score_09 + $score_10;
+    echo '<br>';
+    echo ($score_01 + $score_02 + $score_03 + $score_04 +$score_05 + $score_06 + $score_07 + $score_08 + $score_09 + $score_10 ) / 10;
+    ?>
+  </p>
+</body>
+</html>
